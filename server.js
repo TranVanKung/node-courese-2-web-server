c00893cc1a0f7f8dae960362a30432042affd67c
Your Name <trancongtr1071997@gmail.com>
2018-04-12 16:16:13 +0700

    second commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -54,6 +55,6 @@ app.get('/bad', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log('Server is up in port 3000');
+app.listen(port, () => {
+  console.log(`Server is up in port ${port}`);
 });
